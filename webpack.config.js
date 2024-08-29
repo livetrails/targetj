66163e0e79b61e90fa47928e15f4a854e3db96bc
wasfi2 <wasfi2@hotmail.com>
2024-08-29 03:11:29 -0700

    minor change in App

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
         environment: {
           arrowFunction: false
         },        
-        path: path.resolve(__dirname, '../targetj/jslib'),
+        path: path.resolve(__dirname, './dist'),
         filename: 'targetj.js'
     },
     module: {
